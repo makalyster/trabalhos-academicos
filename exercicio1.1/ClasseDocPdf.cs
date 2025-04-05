b8656c547d833766a0daae30047ad7103a974e79
makalyster <makalyster.sv@gmail.com>
2025-04-04 21:17:41 -0300

    Add classe DocPDF com marca d'água override

--- exercicio1.1/ClasseDocPdf.cs
+++ exercicio1.1/ClasseDocPdf.cs
@@ -1,12 +1,33 @@
-/*
 public class DocumentoPdf:Documento
 {
+    //atributos especificos
+    public string Conteudo { get; set; } 
+    public string MarcaDagua { get; private set; } 
+
+    // Construtor
+    public DocumentoPdf(string titulo, string autor, DateTime dataCriacao, string conteudo): 
+    base(titulo, autor, dataCriacao)
+    {
+        Conteudo = conteudo;
+    }
+
+    //metodo q sera substituido
     public override void Imprimir()
     {
-        Console.WriteLine($"Documento de Pdf");
-        Console.WriteLine($"    Titulo: {Titulo}");
-        Console.WriteLine($"    Titulo: {Autor}");
-        Console.WriteLine($"    Titulo: {DataCriacao}");
+
+        base.Imprimir(); 
+        Console.WriteLine($"Conteudo PDF: {Conteudo}\nMarca d'agua: {MarcaDagua}");
+    }
+
+    // Conteudo formatado e marca d'agua
+    public override string ConteudoFormatado()
+    {
+        return $"{Conteudo} \n[Marca d'agua: {MarcaDagua}]";
+    }
+
+    // metodo especificos 
+    public void AdicionarMarcaDagua(string texto)
+    {
+        MarcaDagua = texto;
     }
 }
-*/
