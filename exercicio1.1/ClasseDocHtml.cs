b09d5332152ff042a195c35e2225ac8d8da3a5b2
makalyster <makalyster.sv@gmail.com>
2025-04-04 21:24:52 -0300

    Add classe DocHTML com override

--- exercicio1.1/ClasseDocHtml.cs
+++ exercicio1.1/ClasseDocHtml.cs
@@ -1,12 +1,23 @@
-/*
+
 public class DocumentoHtml:Documento
 {
+    public string Html { get; set; } // conteudo do HTML
+
+    // Construtor
+    public DocumentoHtml(string titulo, string autor, DateTime dataCriacao, string html): 
+    base(titulo, autor, dataCriacao)
+    {
+        Html = html;
+    }
+
     public override void Imprimir()
     {
-        Console.WriteLine($"Documento de Html");
-        Console.WriteLine($"    Titulo: {Titulo}");
-        Console.WriteLine($"    Titulo: {Autor}");
-        Console.WriteLine($"    Titulo: {DataCriacao}");
+        base.Imprimir(); 
+        Console.WriteLine($"HTML:\n{Html}"); 
+    }
+    
+    public override string ConteudoFormatado()
+    {
+        return $"<html><body>{Html}</body></html>";
     }
 }
-*/
