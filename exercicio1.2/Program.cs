086699c72685aee2ec00df893ce287ffecc55bcc
makalyster <makalyster.sv@gmail.com>
2025-04-05 18:31:23 -0300

    Add Programa com exemplos

--- exercicio1.2/Program.cs
+++ exercicio1.2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        List<Notificacao> notificacoes = new List<Notificacao>
+        {
+            new NotificacaoEmail { Destinatario = "alice@email.com", Mensagem = "Bem-vinda!", DataEnvio = DateTime.Now },
+            new NotificacaoSMS { Destinatario = "+5511999999999", Mensagem = "Seu código é 1234", DataEnvio = DateTime.Now },
+            new NotificacaoPush { Destinatario = "usuário123", Mensagem = "Você tem uma nova conquista!", DataEnvio = DateTime.Now }
+        };
+
+        foreach (var notificacao in notificacoes)
+        {
+            notificacao.Enviar();
+            Console.WriteLine();
+        }
+    }
+}
