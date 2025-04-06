359bc66712dbcfaa0a1413ccb2315076762e984b
makalyster <makalyster.sv@gmail.com>
2025-04-06 05:35:16 -0300

    Add interface IArmazenamento

---
+++ exercicio1.4/IArmazenamento.cs
@@ -0,0 +1,7 @@
+public interface IArmazenamento
+{
+    bool Salvar(string nome, byte[] dados);
+    byte[] Carregar(string nome);
+    bool Excluir(string nome);
+    List<string> ListarArquivos();
+}
